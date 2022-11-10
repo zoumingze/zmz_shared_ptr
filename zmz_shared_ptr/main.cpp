5d1b41203e21f04ab4144c673c35b335469345cb
mingze.zou <mingze.zou@nio.com>
2022-11-10 18:34:07 +0800

    init

---
+++ zmz_shared_ptr/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  zmz_shared_ptr
+//
+//  Created by mingze.zou on 2022/11/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ zmz_shared_ptr/shared_ptr.h
@@ -0,0 +1,12 @@
+//
+//  shared_ptr.h
+//  zmz_shared_ptr
+//
+//  Created by mingze.zou on 2022/11/10.
+//
+
+#ifndef shared_ptr_h
+#define shared_ptr_h
+
+
+#endif /* shared_ptr_h */
